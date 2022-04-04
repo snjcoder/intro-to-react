68beaae44fed4cb0ce49d6dbcf07a4c0df899b1e
Neetha <neethacohort@gmail.com>
2022-04-04 13:18:58 -0600

    changed values and space

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import Displayer from "./components/Displayer";
 import Adder from "./components/Adder";
 
 function App(props) {
-  const [count, setCount] = useState(4);
-  const [count2, setCount2] = useState(4);
+  const [count, setCount] = useState(10);
+  const [count2, setCount2] = useState(6);
   return (
     <div>
       <h1>Hello C8!{count}</h1>
       <button
       onClick={() => {
+        setCount(count +1)
         setCount((curr) => {return curr +1});
       }}
         >
           PLUS ONE
           </button> 
+          <pre>
           <Displayer value={count2}/> 
+
+          </pre>
           <Adder theFuction={setCount2}/>   
     </div>
   );
